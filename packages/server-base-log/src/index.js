77b0788301cb93ed7e51b9b880ddb3d5ff24283c
James Kyburz <james.kyburz@gmail.com>
2017-09-03 22:53:44 +0200

    create only one pino log

--- packages/server-base-log/src/index.js
+++ packages/server-base-log/src/index.js
@@ -1,37 +1,42 @@
 const pino = require('pino')
 
+let log
+
 module.exports = createLog
 
 function createLog (name, opt) {
-  const level = process.env.LOG_LEVEL || 'debug'
+  if (!log) {
+    const level = process.env.LOG_LEVEL || 'debug'
 
-  opt = Object.assign({}, {
-    safe: true,
-    serializers: {
-      req: pino.stdSerializers.req,
-      res: pino.stdSerializers.res,
-      q: pino.stdSerializers.req,
-      r: pino.stdSerializers.res
-    },
-    level: level
-  }, opt, { name })
+    opt = Object.assign({}, {
+      safe: true,
+      serializers: {
+        req: pino.stdSerializers.req,
+        res: pino.stdSerializers.res,
+        q: pino.stdSerializers.req,
+        r: pino.stdSerializers.res
+      },
+      level: level
+    }, opt)
 
-  const pretty = process.env.LOG_PRETTY
-    ? pino.pretty(opt.pretty)
-    : undefined
+    const pretty = process.env.LOG_PRETTY
+      ? pino.pretty(opt.pretty)
+      : undefined
 
-  if (pretty) {
-    pretty.pipe(process.stdout)
-    pretty.on('error', reportError)
+    if (pretty) {
+      pretty.pipe(process.stdout)
+      pretty.on('error', reportError)
+    }
+
+    log = pino(opt, pretty)
+    log.on('error', reportError)
   }
 
-  const log = pino(opt, pretty)
-  log.on('error', reportError)
+  const child = log.child({ name })
+  const methods = ['info', 'fatal', 'debug', 'error', 'trace', 'warn']
+  methods.forEach((key) => { child[key] = child[key].bind(child) })
 
-  const logMethods = ['info', 'fatal', 'debug', 'error', 'trace', 'warn']
-  logMethods.forEach((key) => { log[key] = log[key].bind(log) })
-
-  return log
+  return child
 }
 
 function reportError (err) {
